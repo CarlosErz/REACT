a183ad18a8be8651306b027bf9b60ef0e249689d
CarlosErz <rosariosalda25@gmail.com>
2023-01-19 17:19:49 -0600

    new  url img

--- Cast/src/App.jsx
+++ Cast/src/App.jsx
@@ -3,7 +3,16 @@ import { ServiceCard } from './ServiceCard.jsx'
 import { SectionName } from './SectionName'
 import { HeaderApp } from './HeaderApp'
 import { Navbar } from './Navbar'
-import './assets/logo.svg';
+import '../assets/logo.svg';
+import '../assets/minilogo.svg';
+import '../assets/inicio.svg';
+import '../assets/Servicios.svg'; 
+import '../assets/Sobre nosotros.svg';
+import '../assets/productos.svg';
+import '../assets/Reparacion de celulares.svg';
+import '../assets/Computadoras.svg';
+import '../assets/Desbloqueos.svg';
+import '../assets/Reparacion de celulares.svg';
 
 const card = [
   {
