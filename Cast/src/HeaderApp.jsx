c1a2b12b0eae14bf0f2961d5d4dd4fb35988ce4f
iAm6oy <65881793+iAm6oy@users.noreply.github.com>
2023-01-19 16:21:45 -0600

    Update HeaderApp.jsx

--- Cast/src/HeaderApp.jsx
+++ Cast/src/HeaderApp.jsx
@@ -2,11 +2,11 @@ export function HeaderApp() {
   return (
     <div className="Header">
       <section className="Header-flex">
-        <img src="./assets/logo.svg" alt="" className="Header-logo" />
+        <img type="image/svg+xml" src="./logo.svg" alt="" className="Header-logo" />
         <p className="Header-info">
         Servicios de calidad y rapidez
         </p>
-        <a href="https://wa.me/528682592253" className="Header-btn">Contactanos</a>
+        <a href="https://wa.me/528682592253" className="Header-btn" target="_blank">Contactanos</a>
         <p className="Header-bt">!Cotiza sin compromiso!</p>
       </section>
     </div>
