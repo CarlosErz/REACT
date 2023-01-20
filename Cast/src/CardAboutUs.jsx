d34b1efbeddfbcf2e49d4a155ad56cd2cbc7f10f
iAm6oy <65881793+iAm6oy@users.noreply.github.com>
2023-01-20 01:56:15 -0600

    Create CardAboutUs.jsx

---
+++ Cast/src/CardAboutUs.jsx
@@ -0,0 +1,13 @@
+import logo from '/public/minilogo.svg';
+export function CardAboutUs() {
+  return (
+    <div className="CardAboutUs">
+      <img type="image/svg+xml" src={logo} alt="LOGO" />
+        <p>
+          Servicios de calidad y rapidez
+        </p>
+        <p>!Cotiza sin compromiso!</p>
+    </div>
+  )
+
+}
