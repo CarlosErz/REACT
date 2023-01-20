afb8c103bd0c92132d8cbe41c17758264bfeba57
CarlosErz <rosariosalda25@gmail.com>
2023-01-19 18:13:05 -0600

    nav bar

--- Cast/src/Navbar.jsx
+++ Cast/src/Navbar.jsx
@@ -1,25 +1,29 @@
-
+import inicio from "/src/assets/Productos.svg"
+import servicios from "/src/assets/Servicios.svg"
+import sobreNosotros from "/src/assets/Sobre nosotros.svg"
+import productos from "/src/assets/Productos.svg"
+import "/src/assets/Servicios.svg"
 export function Navbar() {
   return (
     <nav className="Nav">
       <ul className="Nav-lis">
         <li className="Nav-item">
-          <a href="#"><img src="src/assets/inicio.svg" alt="Inicio img" className="Nav-icon" />
+          <a href="#"><img src={inicio} alt="Inicio img" className="Nav-icon" />
             INICIO
           </a>
         </li>
         <li className="Nav-item">
-          <a href="#Servicios" ><img src="src/assets/Servicios.svg" alt="Servicios img" className="Nav-icon" />
+          <a href="#Servicios" ><img src={servicios} alt="Servicios img" className="Nav-icon" />
             SERVICIOS
           </a>
         </li>
         <li className="Nav-item">
-          <a href="#Sobre nosotros" ><img src="src/assets/Sobre nosotros.svg" alt="Sobre nosotros img" className="Nav-icon" />
+          <a href="#Sobre nosotros" ><img src={sobreNosotros} alt="Sobre nosotros img" className="Nav-icon" />
             SOBRE NOSOTROS
           </a>
         </li>
         <li className="Nav-item">
-          <a href="#"><img src="src/assets/productos.svg" alt="Productos img" className="Nav-icon" />
+          <a href="#"><img src={productos} alt="Productos img" className="Nav-icon" />
             PRODUCTOS
           </a>
         </li>
