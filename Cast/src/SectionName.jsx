98b82fb3b6b646fe06059fdbc4222b20acbba5f8
CarlosErz <rosariosalda25@gmail.com>
2023-01-19 18:05:19 -0600

    img

--- Cast/src/SectionName.jsx
+++ Cast/src/SectionName.jsx
@@ -6,7 +6,7 @@ export function SectionName({ Section,CardId ,IconId}) {
     <div className="Section" id={CardId}>
       <h2 className="Section-name">{Section}</h2>
       <i className="Section-icon">
-        <img className={IconId} src= {`src/assets/${Section}.svg`} alt={`${Section}`} />
+        <img className={IconId} src= {`/src/assets/${Section}.svg`} alt={`${Section}`} />
       </i>
     </div>
   )
