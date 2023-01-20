a329dd69a6d4cdc69758a912225ec5ee9766e853
iAm6oy <65881793+iAm6oy@users.noreply.github.com>
2023-01-20 17:17:55 -0600

    s

--- Cast/src/main.jsx
+++ Cast/src/main.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { App } from './App.jsx';
+import { Erro404 } from './404.jsx';
+import './404.cc'
 import './index.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
    <App/>
 )
-
+const Error = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+   <Error/>
+)
