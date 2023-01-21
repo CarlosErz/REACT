07890974187025a60e617dcad1cfc47151b36f1d
CarlosErz <rosariosalda25@gmail.com>
2023-01-21 08:40:19 -0600

    advert and errores

--- Cast/src/main.jsx
+++ Cast/src/main.jsx
@@ -7,3 +7,4 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
    <App/>
 )
+console.log("%c ¡Cuidado! No intentes modificar el código de esta página. Puedes causar daños graves al sistema. Si tienes alguna duda, contacta al administrador.", "color:#8F0F07; font-weight: bold; font-size:25px;");
